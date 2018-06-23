7a5a11afaef3a2be7fcf96d9d71d60db49baf38a
RoOmuloO <romuloroger.contatos@gmail.com>
2018-06-23 20:25:45 -0300

    Including User in Bank

--- banco_RM-e_Script.sql
+++ banco_RM-e_Script.sql
@@ -1,148 +1,203 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `rm_e` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `rm_e`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: rm_e
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.32-MariaDB
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema rm_e
--- -----------------------------------------------------
+--
+-- Table structure for table `consulta`
+--
 
--- -----------------------------------------------------
--- Schema rm_e
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `rm_e` DEFAULT CHARACTER SET utf8 ;
-USE `rm_e` ;
-
--- -----------------------------------------------------
--- Table `rm_e`.`usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `rm_e`.`usuario` (
-  `idusuario` INT(11) NOT NULL AUTO_INCREMENT,
-  `login` VARCHAR(45) NOT NULL,
-  `senha` VARCHAR(45) NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NULL DEFAULT NULL,
-  `tipo` INT(11) NOT NULL,
-  `cpf` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`idusuario`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `rm_e`.`consulta`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `rm_e`.`consulta` (
-  `idconsulta` INT(11) NOT NULL AUTO_INCREMENT,
-  `data` VARCHAR(45) NULL DEFAULT NULL,
-  `local` VARCHAR(45) NOT NULL,
-  `hora` VARCHAR(45) NULL DEFAULT NULL,
-  `usuario_medico` INT(11) NOT NULL,
-  `usuario_paciente` INT(11) NOT NULL,
+DROP TABLE IF EXISTS `consulta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `consulta` (
+  `idconsulta` int(11) NOT NULL AUTO_INCREMENT,
+  `data` varchar(45) DEFAULT NULL,
+  `local` varchar(45) NOT NULL,
+  `hora` varchar(45) DEFAULT NULL,
+  `usuario_medico` int(11) NOT NULL,
+  `usuario_paciente` int(11) NOT NULL,
   PRIMARY KEY (`idconsulta`),
-  INDEX `fk_consulta_usuario1_idx` (`usuario_medico` ASC),
-  INDEX `fk_consulta_usuario2_idx` (`usuario_paciente` ASC),
-  CONSTRAINT `fk_consulta_usuario1`
-    FOREIGN KEY (`usuario_medico`)
-    REFERENCES `rm_e`.`usuario` (`idusuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_consulta_usuario2`
-    FOREIGN KEY (`usuario_paciente`)
-    REFERENCES `rm_e`.`usuario` (`idusuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_consulta_usuario1_idx` (`usuario_medico`),
+  KEY `fk_consulta_usuario2_idx` (`usuario_paciente`),
+  CONSTRAINT `fk_consulta_usuario1` FOREIGN KEY (`usuario_medico`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_consulta_usuario2` FOREIGN KEY (`usuario_paciente`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `consulta`
+--
 
--- -----------------------------------------------------
--- Table `rm_e`.`exame`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `rm_e`.`exame` (
-  `idexame` INT(11) NOT NULL AUTO_INCREMENT,
-  `tipo` VARCHAR(45) NOT NULL,
-  `descricao` VARCHAR(45) NOT NULL,
-  `consulta_idconsulta` INT(11) NOT NULL,
+LOCK TABLES `consulta` WRITE;
+/*!40000 ALTER TABLE `consulta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `consulta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exame`
+--
+
+DROP TABLE IF EXISTS `exame`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exame` (
+  `idexame` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(45) NOT NULL,
+  `descricao` varchar(45) NOT NULL,
+  `consulta_idconsulta` int(11) NOT NULL,
   PRIMARY KEY (`idexame`),
-  INDEX `fk_exame_consulta1_idx` (`consulta_idconsulta` ASC),
-  CONSTRAINT `fk_exame_consulta1`
-    FOREIGN KEY (`consulta_idconsulta`)
-    REFERENCES `rm_e`.`consulta` (`idconsulta`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_exame_consulta1_idx` (`consulta_idconsulta`),
+  CONSTRAINT `fk_exame_consulta1` FOREIGN KEY (`consulta_idconsulta`) REFERENCES `consulta` (`idconsulta`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `exame`
+--
 
--- -----------------------------------------------------
--- Table `rm_e`.`medicamento`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `rm_e`.`medicamento` (
-  `idmedicamento` INT(11) NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  `descricao` VARCHAR(45) NULL DEFAULT NULL,
-  `tipo` VARCHAR(45) NULL DEFAULT NULL,
-  `manipuladoura` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`idmedicamento`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+LOCK TABLES `exame` WRITE;
+/*!40000 ALTER TABLE `exame` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exame` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `medicamento`
+--
 
--- -----------------------------------------------------
--- Table `rm_e`.`receita`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `rm_e`.`receita` (
-  `idreceita` INT(11) NOT NULL AUTO_INCREMENT,
-  `usada` TINYINT(4) NULL DEFAULT '0',
-  `cancelada` TINYINT(4) NULL DEFAULT '0',
-  `consulta_idconsulta` INT(11) NOT NULL,
-  `hora` VARCHAR(45) NOT NULL,
-  `data` VARCHAR(45) NOT NULL,
-  `instrucoes_uso` VARCHAR(45) NOT NULL,
+DROP TABLE IF EXISTS `medicamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medicamento` (
+  `idmedicamento` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `descricao` varchar(45) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  `manipuladoura` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idmedicamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medicamento`
+--
+
+LOCK TABLES `medicamento` WRITE;
+/*!40000 ALTER TABLE `medicamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medicamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `receita`
+--
+
+DROP TABLE IF EXISTS `receita`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `receita` (
+  `idreceita` int(11) NOT NULL AUTO_INCREMENT,
+  `usada` tinyint(4) DEFAULT '0',
+  `cancelada` tinyint(4) DEFAULT '0',
+  `consulta_idconsulta` int(11) NOT NULL,
+  `hora` varchar(45) NOT NULL,
+  `data` varchar(45) NOT NULL,
+  `instrucoes_uso` varchar(45) NOT NULL,
   PRIMARY KEY (`idreceita`),
-  INDEX `fk_receita_consulta1_idx` (`consulta_idconsulta` ASC),
-  CONSTRAINT `fk_receita_consulta1`
-    FOREIGN KEY (`consulta_idconsulta`)
-    REFERENCES `rm_e`.`consulta` (`idconsulta`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_receita_consulta1_idx` (`consulta_idconsulta`),
+  CONSTRAINT `fk_receita_consulta1` FOREIGN KEY (`consulta_idconsulta`) REFERENCES `consulta` (`idconsulta`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `receita`
+--
 
--- -----------------------------------------------------
--- Table `rm_e`.`receita_has_medicamento`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `rm_e`.`receita_has_medicamento` (
-  `receita_idreceita` INT(11) NOT NULL,
-  `medicamento_idmedicamento` INT(11) NOT NULL,
-  `vendido` TINYINT(4) NOT NULL DEFAULT '0',
-  `instrucoes_uso` VARCHAR(45) NULL DEFAULT NULL,
-  `dataVenda` VARCHAR(45) NULL DEFAULT NULL,
-  `horaVenda` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`receita_idreceita`, `medicamento_idmedicamento`),
-  INDEX `fk_receita_has_medicamento_medicamento1_idx` (`medicamento_idmedicamento` ASC),
-  INDEX `fk_receita_has_medicamento_receita1_idx` (`receita_idreceita` ASC),
-  CONSTRAINT `fk_receita_has_medicamento_medicamento1`
-    FOREIGN KEY (`medicamento_idmedicamento`)
-    REFERENCES `rm_e`.`medicamento` (`idmedicamento`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_receita_has_medicamento_receita1`
-    FOREIGN KEY (`receita_idreceita`)
-    REFERENCES `rm_e`.`receita` (`idreceita`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+LOCK TABLES `receita` WRITE;
+/*!40000 ALTER TABLE `receita` DISABLE KEYS */;
+/*!40000 ALTER TABLE `receita` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `receita_has_medicamento`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `receita_has_medicamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `receita_has_medicamento` (
+  `receita_idreceita` int(11) NOT NULL,
+  `medicamento_idmedicamento` int(11) NOT NULL,
+  `vendido` tinyint(4) NOT NULL DEFAULT '0',
+  `instrucoes_uso` varchar(45) DEFAULT NULL,
+  `dataVenda` varchar(45) DEFAULT NULL,
+  `horaVenda` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`receita_idreceita`,`medicamento_idmedicamento`),
+  KEY `fk_receita_has_medicamento_medicamento1_idx` (`medicamento_idmedicamento`),
+  KEY `fk_receita_has_medicamento_receita1_idx` (`receita_idreceita`),
+  CONSTRAINT `fk_receita_has_medicamento_medicamento1` FOREIGN KEY (`medicamento_idmedicamento`) REFERENCES `medicamento` (`idmedicamento`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_receita_has_medicamento_receita1` FOREIGN KEY (`receita_idreceita`) REFERENCES `receita` (`idreceita`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `receita_has_medicamento`
+--
+
+LOCK TABLES `receita_has_medicamento` WRITE;
+/*!40000 ALTER TABLE `receita_has_medicamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `receita_has_medicamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `idusuario` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `tipo` int(11) NOT NULL,
+  `cpf` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idusuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Admin','123456','admin','admin@admin.com',4,'teste'),(3,'Medico','123456','testeMedico','teste',1,'teste'),(4,'Paciente','123456','testePaciente','teste',3,'teste'),(5,'Farmaceutico','123456','testeFarma','testeFarma',2,'testeFarma');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-23 20:24:48
